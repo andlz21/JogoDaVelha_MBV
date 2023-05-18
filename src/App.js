1b60e49cc5752b32bd0744cf840edb0069ea7ecc
andlz <andrelsilvadocarmo@gmail.com>
2023-05-18 20:18:13 -0300

    Criado e Adicionado o Menu no App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Sobre from './Objects/Sobre';
 import LogoTicTacToe from './Objects/Logo';
+import Menu from './Objects/Menu';
 
 function App() {
 	return (
 		<>
 			<LogoTicTacToe />
 			<Sobre />
+			<Menu />
 		</>
 	);
 }
---
+++ src/Objects/Menu/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Menu.css'
+
+const Menu = () => {
+   return (
+      <a href="#Menu" className='menu'><span className='menu_center'>Menu</span></a>
+   )
+}
+
+export default Menu;
