d8782459b0d8895189e7a8a1e6a2161909fed2f1
andlz <andrelsilvadocarmo@gmail.com>
2023-05-23 17:00:13 -0300

    Criamos checkbox e deixamos Input mais simples

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 
 import Header from './Components/Header';
 import Hashtag from './Components/Hashtag';
-import InputCheckbox from './Objects/Input_Checkbox';
+import Checkbox from './Objects/Checkbox';
 
 const App = () => {
 	return (
 		<main className='app'>
 			<Header />
 			<Hashtag />
-			<InputCheckbox id="showEvents" value="showEvents" content="Mostrar eventos" />
+			<Checkbox id="showEvents" value="showEvents" type='checkbox' content="Mostrar eventos" />
 		</main>
 	);
 };
